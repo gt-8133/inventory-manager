692a6e1d3c75156991de1a8bafd8883ae5976d46
Ben Kucera <14625260+Bkucera@users.noreply.github.com>
2018-09-15 00:22:45 -0400

    update

--- src/main.js
+++ src/main.js
@@ -1,20 +1,20 @@
-import Vue from 'vue';
-import App from './App.vue';
-import Vuetify from 'vuetify';
-import 'vuetify/dist/vuetify.css';
-import router from './router';
-import Promise from 'bluebird';
-import ApolloClient from 'apollo-client';
-import VueApollo from 'vue-apollo';
-import { InMemoryCache } from 'apollo-cache-inmemory';
-import { HttpLink } from 'apollo-link-http';
-import { createMockClient } from './mock';
-import * as _ from 'lodash';
+import Vue from 'vue'
+import App from './App.vue'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.css'
+import router from './router'
+import Promise from 'bluebird'
+import ApolloClient from 'apollo-client'
+import VueApollo from 'vue-apollo'
+import { InMemoryCache } from 'apollo-cache-inmemory'
+import { HttpLink } from 'apollo-link-http'
+import { createMockClient } from './mock'
+import * as _ from 'lodash'
 
 const start = () => {
   Promise.try(() => {
     if (process.env.MOCKED) {
-      return createMockClient();
+      return createMockClient()
     }
     // Paste your endpoint for the Simple API here.
     // Info: https://github.com/graphcool-examples/vue-apollo-instagram-example#2-create-graphql-api-with-graphcool
@@ -23,9 +23,9 @@ const start = () => {
         uri: 'http://localhost:60000/simple/v1/cjllndxls00020107o32kr4h3'
       }),
       cache: new InMemoryCache()
-    });
+    })
   }).then(apolloClient => {
-    Vue.use(VueApollo);
+    Vue.use(VueApollo)
     Vue.use(Vuetify, {
       theme: {
         primary: '#052d49',
@@ -33,21 +33,21 @@ const start = () => {
         accent: '#8c9eff',
         error: '#b71c1c'
       }
-    });
+    })
 
     const apolloProvider = new VueApollo({
       defaultClient: apolloClient
-    });
+    })
 
     new Vue({
       el: '#app',
       router,
       apolloProvider,
       render: h => h(App)
-    });
-  });
-};
+    })
+  })
+}
 
 // Start the app
 
-start();
+start()
