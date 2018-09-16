197831fe6f5676cd6c831708532d5dafdf19d4ee
Ben Kucera <14625260+Bkucera@users.noreply.github.com>
2018-09-16 02:43:46 -0400

    update comment format

--- scripts/videoComment.js
+++ scripts/videoComment.js
@@ -3,5 +3,5 @@ const bot = require('circle-github-bot').create()
 
 bot.comment(`
 <h3>${bot.env.commitMessage}</h3>
-:clapper:<strong>${bot.artifactLink('cypress/videos/main.spec.js.mp4', 'cypress test video')}</strong>
+:clapper: <strong>${bot.artifactLink('cypress/videos/main.spec.js.mp4', 'cypress test video')}</strong>
 `)
