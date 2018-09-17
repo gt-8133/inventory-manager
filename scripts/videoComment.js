32776c74a3ff7d3e5a8c7e8da8125cc85ce80d7b
Ben Kucera <14625260+Bkucera@users.noreply.github.com>
2018-09-16 20:27:17 -0400

    ci record script

--- scripts/videoComment.js
+++ scripts/videoComment.js
@@ -1,7 +1,16 @@
 // eslint-disable-next-line
 const bot = require('circle-github-bot').create()
+const fs = require('fs')
 
-bot.comment(`
-<h3>${bot.env.commitMessage}</h3>
-:clapper: <strong>${bot.artifactLink('cypress/videos/main.spec.js.mp4', 'cypress test video')}</strong>
+fs.readFile('/tmp/pass', (err) => {
+  if (err) {
+    return bot.comment(`
+  <h3>${bot.env.commitMessage}</h3>
+  :x: :clapper: <strong>${bot.artifactLink('cypress/videos/main.spec.js.mp4', 'cypress test video')}</strong>
 `)
+  }
+  return bot.comment(`
+  <h3>${bot.env.commitMessage}</h3>
+  :white_check_mark: :clapper: <strong>${bot.artifactLink('cypress/videos/main.spec.js.mp4', 'cypress test video')}</strong>
+  `)
+})
