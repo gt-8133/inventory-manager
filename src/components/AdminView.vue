6ddd32c29c47248fc22f3b96a82d7a4918105e11
Saqib Ali <saqib.muhammad.ali@gmail.com>
2018-09-21 17:14:00 -0400

    Added slide-x-transition for login view and dashboard view. (#73)

--- src/components/AdminView.vue
+++ src/components/AdminView.vue
@@ -1,233 +1,235 @@
 <template>
-  <div>
-    <v-toolbar
-      flat
-      color="white"
-    >
-      <v-toolbar-title />
-      <v-text-field
-        v-model="search"
-        append-icon="search"
-        label="Search"
-        single-line
-        hide-details
-      />
-      <v-spacer />
-      <v-btn
-        color="primary"
-        @click="newItem()"
+  <v-slide-x-transition>
+    <div>
+      <v-toolbar
+        flat
+        color="white"
       >
-        <v-icon
-          dark
-          left
-          tabindex="1"
-        >add_circle</v-icon>
-        New Item
-      </v-btn>
-      <v-dialog
-        v-model="dialog"
-        width="500"
-        :fullscreen="this.$vuetify.breakpoint.xsOnly"
-        @keydown.esc="close()"
-      >
-        <v-card @keydown.native.enter="save">
-          <v-card-title>
-            <span class="headline">{{ formTitle }}</span>
-          </v-card-title>
+        <v-toolbar-title />
+        <v-text-field
+          v-model="search"
+          append-icon="search"
+          label="Search"
+          single-line
+          hide-details
+        />
+        <v-spacer />
+        <v-btn
+          color="primary"
+          @click="newItem()"
+        >
+          <v-icon
+            dark
+            left
+            tabindex="1"
+          >add_circle</v-icon>
+          New Item
+        </v-btn>
+        <v-dialog
+          v-model="dialog"
+          width="500"
+          :fullscreen="this.$vuetify.breakpoint.xsOnly"
+          @keydown.esc="close()"
+        >
+          <v-card @keydown.native.enter="save">
+            <v-card-title>
+              <span class="headline">{{ formTitle }}</span>
+            </v-card-title>
 
-          <v-card-text>
-            <v-container grid-list-md>
-              <v-layout wrap>
-                <v-flex
-                  xs12
-                  align-center
-                  justify-center
-                  layout
-                  text-xs-center
-                >
-                  <v-badge
-                    right
-                    bottom
-                    overlap
+            <v-card-text>
+              <v-container grid-list-md>
+                <v-layout wrap>
+                  <v-flex
+                    xs12
+                    align-center
+                    justify-center
+                    layout
+                    text-xs-center
                   >
-                    <v-avatar
-                      slot="badge"
-                      :tile="false"
-                      :size="35"
-                      color="grey darken-1"
-                      style="right:3px;bottom:3px"
-                      @click="form.editImage=true"
+                    <v-badge
+                      right
+                      bottom
+                      overlap
                     >
-                      <v-btn
-                        fab
-                        small
-                        data-test="edit-image"
-                        @click="selectImageUrl()"
-                      ><v-icon
-                        dark
-                        style="font-size:21px"
-                      >edit</v-icon></v-btn>
-                    </v-avatar>
-                    <v-avatar
-                      :tile="false"
-                      :size="100"
-                      color="grey lighten-4"
-                    >
-                      <img
-                        v-if="form.item.imageUrl"
-                        :src="form.item.imageUrl"
-                        alt="image"
+                      <v-avatar
+                        slot="badge"
+                        :tile="false"
+                        :size="35"
+                        color="grey darken-1"
+                        style="right:3px;bottom:3px"
+                        @click="form.editImage=true"
                       >
-                      <v-icon
-                        v-else
-                        large
-                      >fitness_center</v-icon>
-                    </v-avatar>
-                  </v-badge>
-                  <v-text-field
-                    v-show="form.editImage"
-                    ref="imageUrl"
-                    v-model="form.item.imageUrl"
-                    data-test="imageUrl"
-                    validate-on-blur
-                    solo
-                    placeholder="www.example.com/some-image.png"
-                    autofocus
-                    @blur="form.editImage=false"
-                  />
-                </v-flex>
-                <v-flex
-                  xs12
-                  sm6
-                  md4
-                >
-                  <v-text-field
-                    ref="adsf"
-                    v-model="form.item.name"
-                    data-test="name-edit"
-                    label="Item name"
-                  />
-                </v-flex>
-                <v-flex xs12>
-                  <v-textarea
-                    v-model="form.item.description"
-                    solo
-                    name="input-7-4"
-                    label="Description"
-                />                </v-flex>
-                <v-flex
-                  xs12
-                  sm12
-                  md12
-                >
-                  <v-layout wrap>
-                    <v-flex
-                      xs5
-                      sm5
-                      md5
-                    >
-                      <v-text-field
-                        v-model="form.item.quantityUnits"
-                        label="unit type"
-                      />
-                    </v-flex>
-                    <v-flex
-                      xs5
-                      sm5
-                      md5
-                    >
-                      <v-text-field
-                        v-model="form.item.quantity"
-                        :label="form.item.quantityUnits"
-                        type="number"
-                      />
-                    </v-flex>
-                    <v-flex>
-                      <v-switch
-                        v-model="form.item.reusable"
-                        :label="`Reusable`"
-                        light
-                      />
-                    </v-flex>
-                  </v-layout>
-                </v-flex>
-              </v-layout>
-            </v-container>
-          </v-card-text>
+                        <v-btn
+                          fab
+                          small
+                          data-test="edit-image"
+                          @click="selectImageUrl()"
+                        ><v-icon
+                          dark
+                          style="font-size:21px"
+                        >edit</v-icon></v-btn>
+                      </v-avatar>
+                      <v-avatar
+                        :tile="false"
+                        :size="100"
+                        color="grey lighten-4"
+                      >
+                        <img
+                          v-if="form.item.imageUrl"
+                          :src="form.item.imageUrl"
+                          alt="image"
+                        >
+                        <v-icon
+                          v-else
+                          large
+                        >fitness_center</v-icon>
+                      </v-avatar>
+                    </v-badge>
+                    <v-text-field
+                      v-show="form.editImage"
+                      ref="imageUrl"
+                      v-model="form.item.imageUrl"
+                      data-test="imageUrl"
+                      validate-on-blur
+                      solo
+                      placeholder="www.example.com/some-image.png"
+                      autofocus
+                      @blur="form.editImage=false"
+                    />
+                  </v-flex>
+                  <v-flex
+                    xs12
+                    sm6
+                    md4
+                  >
+                    <v-text-field
+                      ref="adsf"
+                      v-model="form.item.name"
+                      data-test="name-edit"
+                      label="Item name"
+                    />
+                  </v-flex>
+                  <v-flex xs12>
+                    <v-textarea
+                      v-model="form.item.description"
+                      solo
+                      name="input-7-4"
+                      label="Description"
+                  />                </v-flex>
+                  <v-flex
+                    xs12
+                    sm12
+                    md12
+                  >
+                    <v-layout wrap>
+                      <v-flex
+                        xs5
+                        sm5
+                        md5
+                      >
+                        <v-text-field
+                          v-model="form.item.quantityUnits"
+                          label="unit type"
+                        />
+                      </v-flex>
+                      <v-flex
+                        xs5
+                        sm5
+                        md5
+                      >
+                        <v-text-field
+                          v-model="form.item.quantity"
+                          :label="form.item.quantityUnits"
+                          type="number"
+                        />
+                      </v-flex>
+                      <v-flex>
+                        <v-switch
+                          v-model="form.item.reusable"
+                          :label="`Reusable`"
+                          light
+                        />
+                      </v-flex>
+                    </v-layout>
+                  </v-flex>
+                </v-layout>
+              </v-container>
+            </v-card-text>
 
-          <v-card-actions>
-            <v-spacer />
-            <v-btn
-              flat
-              color="primary"
-              @click.native="close"
-            >Cancel</v-btn>
-            <v-btn
-              color="primary"
-              flat
-              @click.native="save"
-            >Save</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-    </v-toolbar>
-    <v-data-table
-      :headers="headers"
-      :items="items"
-      :search="search"
-      :loading="$apollo.loading"
-      hide-actions
-      class="elevation-1"
-    >
-      <template
-        slot="items"
-        slot-scope="props"
+            <v-card-actions>
+              <v-spacer />
+              <v-btn
+                flat
+                color="primary"
+                @click.native="close"
+              >Cancel</v-btn>
+              <v-btn
+                color="primary"
+                flat
+                @click.native="save"
+              >Save</v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+      <v-data-table
+        :headers="headers"
+        :items="items"
+        :search="search"
+        :loading="$apollo.loading"
+        hide-actions
+        class="elevation-1"
       >
-        <td>
-          <v-flex
-            xs12
-            align-center
-            justify-left
-            layout
-            text-xs-center
-          >
-            <v-avatar
-              :tile="false"
-              :size="30"
-              color="grey lighten-4"
+        <template
+          slot="items"
+          slot-scope="props"
+        >
+          <td>
+            <v-flex
+              xs12
+              align-center
+              justify-left
+              layout
+              text-xs-center
             >
-              <img
-                v-if="props.item.imageUrl"
-                :src="props.item.imageUrl"
-                alt="image"
+              <v-avatar
+                :tile="false"
+                :size="30"
+                color="grey lighten-4"
               >
-              <v-icon v-else>fitness_center</v-icon>
-            </v-avatar>
-            &nbsp;{{ props.item.name }}
-          </v-flex>
-        </td>
-        <td class="text-xs-right">{{ formatDate(props.item.updatedAt) }}</td>
-        <td class="text-xs-right">{{ props.item.quantityUnits }}: {{ props.item.quantity }}</td>
-        <td class="justify-center layout px-0">
-          <v-icon
-            data-test="edit"
-            med
-            class="mr-2"
-            @click="editItem(props.item)"
-          >
-            edit
-          </v-icon>
-          <v-icon
-            data-test="delete"
-            med
-            @click="deleteItem(props.item)"
-          >
-            delete
-          </v-icon>
-        </td>
-      </template>
-    </v-data-table>
-  </div>
+                <img
+                  v-if="props.item.imageUrl"
+                  :src="props.item.imageUrl"
+                  alt="image"
+                >
+                <v-icon v-else>fitness_center</v-icon>
+              </v-avatar>
+              &nbsp;{{ props.item.name }}
+            </v-flex>
+          </td>
+          <td class="text-xs-right">{{ formatDate(props.item.updatedAt) }}</td>
+          <td class="text-xs-right">{{ props.item.quantityUnits }}: {{ props.item.quantity }}</td>
+          <td class="justify-center layout px-0">
+            <v-icon
+              data-test="edit"
+              med
+              class="mr-2"
+              @click="editItem(props.item)"
+            >
+              edit
+            </v-icon>
+            <v-icon
+              data-test="delete"
+              med
+              @click="deleteItem(props.item)"
+            >
+              delete
+            </v-icon>
+          </td>
+        </template>
+      </v-data-table>
+    </div>
+  </v-slide-x-transition>
 </template>
 <script>
 import gql from 'graphql-tag'
